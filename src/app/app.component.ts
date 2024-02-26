29736f36af9c39b2d08f8174215d86f02eef00c9
luis.Nogales <dashnogales@gmail.com>
2024-02-25 21:12:24 -0700

    fin de seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Hola Mundor';
+//   public counter: number = 10;
+
+//   // increaseBy( value:string ):void{
+//   //   if(value == '+'){
+//   //     this.counter += 1;
+//   //   }else{
+//   //     this.counter -= 1;
+//   //   }
+//   // }
+
+//   increaseBy( value:number ):void{
+
+//     this.counter += value;
+//   }
+// resetValue(){
+//   this.counter = 10;
+// }
 }
