2d70f9d1d72a1bcdf2763d94ec8dcfeaf25590aa
luis.Nogales <dashnogales@gmail.com>
2024-03-03 17:14:42 -0700

    seccion de build a prod

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -16,5 +16,18 @@ export class ListDbzComponent {
     name:'Trunks',
     power: 10
   }]
+
+  //ondeleted = Index value: number;
+@Output()
+public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?:string):void{
+   // console.log({index});
+  if( !id ) return;
+
+  console.log({id})
+   this.onDelete.emit(id);
+
+  }
 
 }
